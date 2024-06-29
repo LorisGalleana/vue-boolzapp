6bcb13adcaf6af4514a79c15bd10c10460a8b5f3
LorisGalleana <loris.galleana@gmail.com>
2024-06-29 19:03:23 +0200

    ESERCIZIO COMPLETATO

--- app.js
+++ app.js
@@ -13,17 +13,21 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
+                            
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -35,17 +39,20 @@ createApp({
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -57,17 +64,20 @@ createApp({
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -79,12 +89,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -96,12 +108,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -113,17 +127,20 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -135,12 +152,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         }
                     ],
                 },
@@ -152,23 +171,36 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownOpen: false
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownOpen: false
                         }
                     ],
                     
                 }
             ],
-            currentContactIndex: 0 // Indice del contatto corrente
+            currentContactIndex: 0,
+            newMessage: '',
+            searchQuery: '',
+            dropdownOpen: false
+        }
+    },
+    computed: {
+        filteredContacts() {
+            return this.contacts.filter(contact => 
+                contact.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+            );
         }
     },
     methods: {
@@ -178,7 +210,27 @@ createApp({
         formatTime(dateString) {
             const dateTime = DateTime.fromFormat(dateString, 'dd/MM/yyyy HH:mm:ss');
             return dateTime.toFormat('HH:mm:ss');
+        },
+        addMessage() {
+            if (this.newMessage !== '' && this.newMessage.length >= 5) {
+                this.contacts[this.currentContactIndex].messages.push({date: DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'), message: this.newMessage, status: 'sent', dropdownOpen: false});
+                setTimeout(() => {this.friendMessage()}, 1000)
+            }
+            this.newMessage = '';
+            
+        },
+        friendMessage() {
+            
+            this.contacts[this.currentContactIndex].messages.push({date: DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss'), message: 'okay!', status: 'received'});
+            
+        },
+        toggleDropdown(message) {
+            message.dropdownOpen = !message.dropdownOpen;
+        },
+        removeMessage(index) {
+            this.contacts[this.currentContactIndex].messages.splice(index, 1);
         }
     },
+
         
 }).mount("#container");
